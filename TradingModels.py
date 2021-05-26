0ad260053b9474251bba4241dd5117a9b8d8db08
Campos <sergio.campos@teradata.com>
2021-05-26 16:36:21 +0200

    Added machine learning models

--- TradingModels.py
+++ TradingModels.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
-# import os
 sys.path.append('OneDrive - Teradata/Documents/SERGIO/Trading/')
 from main import tradingModel
-# import pandas as pd
-# import datetime
-# import matplotlib.pyplot as plt
-# from yahoofinancials import YahooFinancials
 
 # Training model
 idYahoo = 'NDAQ'
@@ -17,16 +12,6 @@ trading = tradingModel(idYahoo=idYahoo, path=path)
 trading.training(varPredict, '2019-01-04', '2021-05-14', True, True)
 
 # Prediction
-trading.prediction(varPredict, '2021-02-17')
-trading.prediction(varPredict, '2021-02-18')
-trading.prediction(varPredict, '2021-02-19')
-trading.prediction(varPredict, '2021-02-20')
-trading.prediction(varPredict, '2021-02-21')
-trading.prediction(varPredict, '2021-02-22')
-trading.prediction(varPredict, '2021-02-23')
-trading.prediction(varPredict, '2021-02-24')
-trading.prediction(varPredict, '2021-02-25')
-trading.prediction(varPredict, '2021-02-26')
 trading.prediction(varPredict, '2021-02-27')
 
 # Metrics prediction
@@ -44,131 +29,3 @@ trading.plotProfitability(dateRentability=dateRentability, dictMarkets=dictMarke
 startDate = '2019-01-04'
 endDate = '2021-05-26'
 trading.plotBollingerBands(startDate, endDate)
-
-
-
-# Training model
-idYahoo = 'AAPL'
-path = 'C:/Users/sc250091/OneDrive - Teradata/Documents/SERGIO/Trading'
-varPredict = 'adjclose'
-trading = tradingModel(idYahoo=idYahoo, path=path)
-trading.training(varPredict, '2019-01-04', '2021-05-14', True, True)
-
-# Prediction
-trading.prediction(varPredict, '2021-02-17')
-trading.prediction(varPredict, '2021-02-18')
-trading.prediction(varPredict, '2021-02-19')
-trading.prediction(varPredict, '2021-02-20')
-trading.prediction(varPredict, '2021-02-21')
-trading.prediction(varPredict, '2021-02-22')
-trading.prediction(varPredict, '2021-02-23')
-trading.prediction(varPredict, '2021-02-24')
-trading.prediction(varPredict, '2021-02-25')
-trading.prediction(varPredict, '2021-02-26')
-trading.prediction(varPredict, '2021-02-27')
-
-# Metrics prediction
-trading.checkPredictions(varPredict)
-
-# BollingerBands
-startDate = '2019-01-04'
-endDate = '2021-05-26'
-trading.plotBollingerBands(startDate, endDate)
-
-
-
-
-
-# Training model
-idYahoo = 'AMZN'
-path = 'C:/Users/sc250091/OneDrive - Teradata/Documents/SERGIO/Trading'
-varPredict = 'adjclose'
-trading = tradingModel(idYahoo=idYahoo, path=path)
-trading.training(varPredict, '2019-01-04', '2021-05-14', True, True)
-
-# Prediction
-trading.prediction(varPredict, '2021-02-17')
-trading.prediction(varPredict, '2021-02-18')
-trading.prediction(varPredict, '2021-02-19')
-trading.prediction(varPredict, '2021-02-20')
-trading.prediction(varPredict, '2021-02-21')
-trading.prediction(varPredict, '2021-02-22')
-trading.prediction(varPredict, '2021-02-23')
-trading.prediction(varPredict, '2021-02-24')
-trading.prediction(varPredict, '2021-02-25')
-trading.prediction(varPredict, '2021-02-26')
-trading.prediction(varPredict, '2021-02-27')
-
-
-# Metrics prediction
-trading.checkPredictions(varPredict)
-
-# BollingerBands
-startDate = '2019-01-04'
-endDate = '2021-05-26'
-trading.plotBollingerBands(startDate, endDate)
-
-
-
-
-# Training model
-idYahoo = 'GOOG'
-path = 'C:/Users/sc250091/OneDrive - Teradata/Documents/SERGIO/Trading'
-varPredict = 'adjclose'
-trading = tradingModel(idYahoo=idYahoo, path=path)
-trading.training(varPredict, '2019-01-04', '2021-05-14', True, True)
-
-# Prediction
-trading.prediction(varPredict, '2021-02-17')
-trading.prediction(varPredict, '2021-02-18')
-trading.prediction(varPredict, '2021-02-19')
-trading.prediction(varPredict, '2021-02-20')
-trading.prediction(varPredict, '2021-02-21')
-trading.prediction(varPredict, '2021-02-22')
-trading.prediction(varPredict, '2021-02-23')
-trading.prediction(varPredict, '2021-02-24')
-trading.prediction(varPredict, '2021-02-25')
-trading.prediction(varPredict, '2021-02-26')
-trading.prediction(varPredict, '2021-02-27')
-
-
-# Metrics prediction
-trading.checkPredictions(varPredict)
-
-# BollingerBands
-startDate = '2019-01-04'
-endDate = '2021-05-26'
-trading.plotBollingerBands(startDate, endDate)
-
-
-
-# Training model
-idYahoo = 'TSLA'
-path = 'C:/Users/sc250091/OneDrive - Teradata/Documents/SERGIO/Trading'
-varPredict = 'adjclose'
-trading = tradingModel(idYahoo=idYahoo, path=path)
-trading.training(varPredict, '2019-01-04', '2021-05-14', True, True)
-
-# Prediction
-trading.prediction(varPredict, '2021-02-17')
-trading.prediction(varPredict, '2021-02-18')
-trading.prediction(varPredict, '2021-02-19')
-trading.prediction(varPredict, '2021-02-20')
-trading.prediction(varPredict, '2021-02-21')
-trading.prediction(varPredict, '2021-02-22')
-trading.prediction(varPredict, '2021-02-23')
-trading.prediction(varPredict, '2021-02-24')
-trading.prediction(varPredict, '2021-02-25')
-trading.prediction(varPredict, '2021-02-26')
-trading.prediction(varPredict, '2021-02-27')
-
-
-# Metrics prediction
-trading.checkPredictions(varPredict)
-
-# BollingerBands
-startDate = '2019-01-04'
-endDate = '2021-05-26'
-trading.plotBollingerBands(startDate, endDate)
-
-
